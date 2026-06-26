df5a37156051194752e88987f372c9fee0401bc7
Mohan-K10 <mohank18m@gmail.com>
2026-06-26 16:12:08 +0530

    Vertex frontEnd

---
+++ FrontEnd/Vertex-cart/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='px-30 py-3'>
+      <h1 className='font-semibold text-3xl'>Vertex Cart</h1>
+    </div>
+  )
+}
+
+export default App
