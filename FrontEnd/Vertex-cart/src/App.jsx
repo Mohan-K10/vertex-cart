00729fe94342869106aa9cf5b8f2bbc9b2b6f876
Mohan-K10 <mohank18m@gmail.com>
2026-06-30 16:33:10 +0530

    fix: Route the pages

--- FrontEnd/Vertex-cart/src/App.jsx
+++ FrontEnd/Vertex-cart/src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
+import { useState } from 'react'
+import AddProduct from './components/AddProduct'
+import SearchProduct from './components/SearchProduct'
+import { Routes, Route } from 'react-router-dom'
+import AddtoCart from './components/AddtoCart'
 
 function App() {
 
+  const [product, setproduct] = useState([])  
+
   return (
+    <>
+    <Routes>
+      {/* <Route path='/' element={<App/>}/> */}  
+      <Route path='/cart' element={<AddtoCart/>}/>
+    </Routes>
+
+
+
+
     <div className='px-30 py-3'>
       <h1 className='font-semibold text-3xl'>Vertex Cart</h1>
+        <AddProduct AllProducts={setproduct}/>
+        <SearchProduct Product={product}/>
+        
     </div>
+    </>
   )
 }
 
