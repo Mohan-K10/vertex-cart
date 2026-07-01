c86b30ac532fe9567d0a476cdfc55df3892427b3
Mohan-K10 <mohank18m@gmail.com>
2026-07-01 13:12:22 +0530

    feat: Connect frontend and Backend with cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,10 +2,13 @@ import express from "express"
 import { configDotenv } from "dotenv"
 import connectDB from "./src/db/db.js"
 import productRoute from "./src/routes/productRoute.js"
+import cors from "cors"
 
 configDotenv()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 await connectDB()
@@ -17,6 +20,10 @@ await connectDB()
 .catch((err)=> {
     console.log("MongoDB failed to Connect ", err)
     process.exit(1)
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
 })
 
 app.use("/api/product", productRoute)
