5b4403a69a6ca607993d161c1b8c14b587c85ce7
Mohan-K10 <mohank18m@gmail.com>
2026-07-01 13:23:00 +0530

    fix: Connect MongoDB through mongoose

--- BackEnd/src/db/db.js
+++ BackEnd/src/db/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async ()=> {
+    //connect MongoDB through mongoose
     try {
         await mongoose.connect(process.env.MONGO_URI)
     } catch (error) {
